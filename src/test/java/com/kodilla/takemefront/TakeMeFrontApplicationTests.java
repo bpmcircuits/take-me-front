ca23bd25e42568324083b029197d28ef1a3652a9
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-07-28 00:09:22 +0200

    first commit

---
+++ src/test/java/com/kodilla/takemefront/TakeMeFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.takemefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeMeFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
